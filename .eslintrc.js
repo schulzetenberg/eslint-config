536637e3259fa6e7e65b13860097ac7909873dbf
Jacob Schulzetenberg <jacob.schulzetenberg@gmail.com>
2019-10-09 15:38:14 -0500

    Inital commit

---
+++ .eslintrc.js
@@ -0,0 +1,74 @@
+module.exports = {
+  "extends": [
+    "airbnb",
+    "prettier",
+    "prettier/react"
+  ],
+  "parser": "babel-eslint",
+  "parserOptions": {
+    "ecmaVersion": 2018,
+    // Can these be removed?
+    "ecmaFeatures": {
+      "impliedStrict": true,
+      "classes": true,
+      "jsx": true
+    }
+  },
+	"env": {
+		"browser": true,
+		"node": true,
+		"jquery": true,
+		"jest": true
+	},
+
+  // Override the airbnb rules here
+  "rules": {
+    "prefer-promise-reject-errors": 0,
+    "no-use-before-define": 0,
+    "max-len": ["error", { "code": 180 }],
+    "jsx-a11y/accessible-emoji": 0,
+
+    // react plugin
+    "react/display-name": 1,
+    "react/no-array-index-key": 0,
+    "react/react-in-jsx-scope": 0,
+    "react/prefer-stateless-function": 0,
+    "react/forbid-prop-types": 0,
+    "react/no-unescaped-entities": 0,
+    "react/require-default-props": 0,
+    "react/jsx-filename-extension": [
+      1,
+      {
+        "extensions": [
+          ".js",
+          ".jsx"
+        ]
+      }
+    ],
+
+    // prettier plugin
+    "prettier/prettier": [
+      "error",
+      {
+        "printWidth": 120,
+        "singleQuote": true,
+        "tabWidth": 2,
+        "useTabs": false,
+        "semi": true,
+        "arrowParens": "always",
+        "trailingComma": "es5",
+        "bracketSpacing": true
+      }
+    ],
+
+    // react-hooks plugin
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+	},
+	"plugins": [
+		"html",
+    "prettier",
+    "react",
+		"react-hooks"
+	]
+};
---
+++ README.md
@@ -0,0 +1,7 @@
+# eslint-config-react
+ Custom eslint config enforcing airbnb, react & prettier rules
+
+### Install
+`
+	$ npx install-peerdeps --dev @schulzetenberg/eslint-config-react
+`
